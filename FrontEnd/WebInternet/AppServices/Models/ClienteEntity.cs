16d432d4f973d81dccd6aa36741ffdd11079edf5
pierriv <pier.rivera9@gmail.com>
2018-09-25 00:52:03 -0500

    front 1

---
+++ FrontEnd/WebInternet/AppServices/Models/ClienteEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace AppServices.Models
+{
+    [Serializable]
+    [DataContract]
+    public class ClienteEntity
+    {
+        [DataMember]
+        public string idCliente { get; set; }
+
+        [DataMember]
+        public string nombre { get; set; }
+
+        [DataMember]
+        public string apellidos { get; set; }
+
+        [DataMember]
+        public string claveInternet { get; set; }
+
+        [DataMember]
+        public List<CuentaEntity> cuentas { get; set; }
+
+    }
+}
---
+++ FrontEnd/WebInternet/AppServices/Models/CuentaEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AppServices.Models
+{
+    [Serializable]
+    [DataContract]
+    public class CuentaEntity
+    {
+        [DataMember]
+        public string idCuenta { get; set; }
+
+        [DataMember]
+        public string numeroCuenta { get; set; }
+
+        [DataMember]
+        public string numeroTarjeta { get; set; }
+
+        [DataMember]
+        public double saldo { get; set; }
+    }
+}
